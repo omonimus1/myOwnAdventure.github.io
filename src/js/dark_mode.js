c4e9325d2c67dca84683e53c098ea04f42e31a24
omonimus1 <davidepollicino2015@gmail.com>
2020-02-27 11:34:14 +0000

    Session vueJs coookies does not work yet for dark theme

--- src/js/dark_mode.js
+++ src/js/dark_mode.js
@@ -1,24 +1,38 @@
+
 var switchStatus = false;
+// 5 day after, expire
+//Vue.$cookies.config('5d')
+
 $("#togBtn").on('change', function() {
     if ($(this).is(':checked')) {
         switchStatus = $(this).is(':checked');
         set_black_theme();// To verify
+        //this.$cookies.set("themeCookie",switchStatus,0);
+        //alert(this.$cookies.get("themeCookie");        
     }
     else {
-       switchStatus = $(this).is(':checked');
-       unset_blacktheme();// To verify
+        unset_blacktheme();// To verify
+        //this.$cookies.set("themeCookie",switchStatus,0);  
+       // alert(this.$cookies.get("themeCookie");      
     }
 });
 
 
+
+
 function set_black_theme()
-{
+{    
     document.body.style.backgroundColor = "grey";
     document.body.style.color = "white";
     colorLinks("black");
+    Console.log(getCookie(switchIsSelected));
 }
 
-
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+  }
 
 function  unset_blacktheme()
 {
