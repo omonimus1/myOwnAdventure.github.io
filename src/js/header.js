1a64118bb81a1f0678eea8ee354ae91204d9826f
omonimus1 <davidepollicino2015@gmail.com>
2020-03-01 13:27:25 +0000

    Updateed source lInk > Link now added dynamically using JS

---
+++ src/js/header.js
@@ -0,0 +1,7 @@
+function changeLink()
+{
+    var link = document.getElementById("source_code_link");
+  
+      link.setAttribute('href', "https://github.com/omonimus1/myOwnAdventure.github.io");
+      return false; 
+}
