f10f772eedea90cbb3acc0253213d3dfcbf84f67
omonimus1 <davidepollicino2015@gmail.com>
2020-03-03 13:05:29 +0000

    Personal information works

--- src/js/story_page.js
+++ src/js/story_page.js
@@ -2,21 +2,32 @@ function getPageTitle()
 {
     var title = document.title;
     alert(title);
-    create_range_element(title);
+    set_parameters(title);
 
 }
 
 
-function create_range_element(title)
+function set_parameters(title)
+{   
+   if(title == "Back home")
+    {
+        var end_flow_story = 100;
+        var creativity = 85;
+        var boringess = 10;
+       
+    }
+    set_value_performance(end_flow_story , creativity ,boringess);
+}
+
+function set_value_performance( flow_story , creativity , boringess )
 {
-    // Create the new node to insert
-    let newNode = document.createElement("INPUT");
-    newNode.setAttribute("type", "range");
+    document.getElementsByTagName("INPUT")[1].setAttribute("value", flow_story);
+    //document.getElementsById("storyFlowValidation").setAttribute
 
-    // Get a reference to the parent node
-    let parentDiv = document.getElementById("valutation").parentNode
+    document.getElementsByTagName("INPUT")[2].setAttribute("value", creativity);
+    document.getElementsByTagName("INPUT")[3].setAttribute("value", boringess);
 
-    // Begin test case [ 1 ] : Existing childElement (all works correctly)
-    let sp2 = document.getElementById("valutation")
-    parentDiv.insertBefore(newNode, sp2)
+ 
+
+    document.getElementById("valutation").style.visibility = "visible";
 }
