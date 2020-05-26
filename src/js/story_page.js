34ba228c0cbc8f684e62d2a83c43e792eb90bb21
Davide Pollicino <davidepollicino2015@gmail.com>
2020-05-26 23:57:50 +0100

    Code review: improves set parameters story_page function

--- src/js/story_page.js
+++ src/js/story_page.js
@@ -1,58 +1,27 @@
-function getPageTitle()
-{
+/* Get title page of our web page */
+function getPageTitle() {
     var title = document.title;
     set_parameters(title);
-
 }
 
-
-function set_parameters(title)
-{   
-    var end_flow_story = 0;
-    var creativity = 0;
-    var boringess = 0; 
-   if(title == "Back home")
-    {
-        end_flow_story = 100;
-        creativity = 64;
-        boringess = 10;
-       
-    }
-    else if(title == "Stranger met Pollicino Father")
-    {   
-        end_flow_story = 60;
-        creativity = 86;
-        boringess = 4;
-    }
-    else if(title == "Pollicino has been stolen")
-    {
-        end_flow_story = 58;
-        creativity = 100;
-        boringess = 2; 
-    }
-    else if(title == "Pollicino was never born")
-    {
-        end_flow_story = 100;
-        creativity = 0;
-        boringess = 100; 
-    }
-    else if(title == "Pollicino Intro")
-    {
-        end_flow_story = 5;
-        creativity = 5;
-        boringess = 0; 
-    }
-    else if(title = "Pollicino is growing Up")
-    {
-        end_flow_story = 10;
-        creativity = 20;
-        boringess = 0; 
-    }
-    set_value_performance(end_flow_story , creativity ,boringess);
+/* Set values of the Flow_story, creativity and boringness */
+function set_parameters(title) {
+    if (title == "Back home") {
+        // Parameters: flow_story, creativity, boringness
+        set_value_performance(100, 64, 10);
+    } else if (title == "Stranger met Pollicino Father")
+        set_value_performance(60, 86, 4);
+    else if (title == "Pollicino has been stolen")
+        set_value_performance(58, 100, 2);
+    else if (title == "Pollicino was never born")
+        set_value_performance(100, 0, 100);
+    else if (title == "Pollicino Intro")
+        set_value_performance(5, 5, 0);
+    else if (title = "Pollicino is growing Up")
+        set_value_performance(10, 20, 0);
 }
 
-function set_value_performance( flow_story , creativity , boringess )
-{
+function set_value_performance(flow_story, creativity, boringess) {
     // Set vlaues to the three different ranges
     document.getElementsByTagName("INPUT")[1].setAttribute("value", flow_story);
     document.getElementsByTagName("INPUT")[2].setAttribute("value", creativity);
